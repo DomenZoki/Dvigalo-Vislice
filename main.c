1506a4ff8c7c7b7ddeecd033aff9e59b41009ccf
DomenZoki <48260900+DomenZoki@users.noreply.github.com>
2023-01-03 16:08:37 +0100

    Vislice
    Program se začne tako, da naključno izbere besedo iz polja različnih besed. V terminalu se izpiše dolžina besede, za vsako črko se izpiše prazno polje. Program nato vpraša uporabnika za črko, ko uporabnik vpiše poljubno črko, program izpiše, če je pravilna ali ne. Če je črka pravilna se prazna polja, na katerih je ta določena črka, nadomestijo s črko, če pa je črka napačna, se uporabniku zmanjša število poskusov in v terminalu se izpiše polje, v katerem so zapisane vse napačne črke, katere je uporabnik vpisal. Igra se konča, če uporabniku zmanjka število poskusov ali če uporabnik ugane vse črke besede.

--- main.c
+++ main.c
@@ -1,221 +1,167 @@
 #include <stdio.h>
+#include <time.h> 
 #include <stdlib.h>
 #include <string.h>
-#include <stdlib.h>
-#include <time.h>
+#include <ctype.h>
 
-void izpisNadstropja(int *trenutnoNadstropje);
-void vnos(int *trenutnoNadstropje);
-void vnosType(char str[100], int *trenutnoNadstropje);
-void konec(int *index);
-void izpisPotnikov(int *stPotnikov);
-void izstopPotnikov(int *trenutnoNadstropje, int *stPotnikov, int index);
+int str_dolzina(char str[]);
+void izpis(char beseda[], int dolzina);
+void update(int stPoskusov, char napacne[]); 
+void vnos(int *stPoskusov, char napacne[], char beseda[], int dolzinaBesede, char *vnesenaCrka, int *napacenVnos);
 
 int main(){
 
-    int trenutnnoNadstropje = 0;
-    int index = 0;
+    printf("\n");
+    printf("VISLICE\n");
+    printf("\n");
 
-    //Nakljucno stevilo potnikov 1-9 vkljucno z uporabnikom
-    int stPotnikov;
-    srand(time(0));
-    stPotnikov = (rand() % 9) + 1;
+    int steviloPoskusov = 9;
 
-    while (index != 1)
+    char besede[10][10] = {"sova", "radio", "zgodovina", "drevo", "bizon", "nafta", "navada", "premog", "cvet", "orel"};
+
+    char napacne[10];
+
+    //Nakljucna beseda
+    int index;
+    srand(time(NULL));
+    index = rand() % 10;
+
+    int dolzina = str_dolzina(besede[index]);
+    printf("Dolzina besede: %i\n", dolzina);
+
+
+    char beseda[dolzina];
+    for (int i = 0; i < dolzina; i++)
     {
-        izpisNadstropja(&trenutnnoNadstropje);
-        izpisPotnikov(&stPotnikov);
-        vnos(&trenutnnoNadstropje);
-        izstopPotnikov(&trenutnnoNadstropje, &stPotnikov, index);
-        izpisNadstropja(&trenutnnoNadstropje);
-        konec(&index);
+        beseda[i] = '_';
     }
-    
-}
 
-void izpisNadstropja(int *trenutnoNadstropje){
-    if(*trenutnoNadstropje == 0){
-        printf("Trenutno ste v pritlicju\n");
-    }else if (*trenutnoNadstropje == -1)
-    {
-        printf("Trenutno ste v kleti B3\n");
-    }else if (*trenutnoNadstropje == -2)
-    {
-        printf("Trenutno ste v kleti B2\n");
-    }else if (*trenutnoNadstropje == -3)
-    {
-        printf("Trenutno ste v kleti B1\n");
-    }
-    
-    
-    else{
-        printf("Trenutno ste na %i. nadstropju\n", *trenutnoNadstropje);
-    }
-}
+    izpis(beseda, dolzina);
 
-void vnos(int *trenutnoNadstropje){
-    char input[100];
-    printf("Vnesite zazeleno nadstropje (B1, B2, B3, P, 1, 2, 3, 4, 5): ");
-    scanf("%s", input);
+    int x;
+    int napacenVnos = 0;
 
-    
+    while(x != 0){
 
-    vnosType(input, trenutnoNadstropje);
-    
-}
+        x = 0;
 
-void vnosType(char str[100], int *trenutnoNadstropje){
-    int kateri;
-    int jeInt = 0, jeChar = 0, jeFloat = 0;
-    int intNum = 0;
-    char character;
-    for (int i = 0; i < strlen(str); i++)
-    {
-        //Ce je stevilo
-        if(((str[i] <= '9'))){
-            jeInt++;
+        char vnesenaCrka;
+        vnos(&steviloPoskusov, napacne, besede[index], dolzina, &vnesenaCrka, &napacenVnos);
+
+        update(steviloPoskusov, napacne);
+
+        for (int i = 0; i < dolzina; i++)
+        {
+            if (besede[index][i] == vnesenaCrka){
+                beseda[i] = vnesenaCrka;
+            }
         }
 
-        //Ce ni stevilo 
-        if(!((str[i] <= '9'))){
-            if(str[i] == '.'){
-                jeFloat++;
-            }else{
-                jeChar++;
+        izpis(beseda, dolzina);
+        
+        while((getchar()) != '\n');
+
+        //Preverimo, ce je uporabnik izpolnil vsa polja
+        for (int i = 0; i < dolzina; i++)
+        {
+            if(beseda[i] == '_'){
+                x++;
             }
+        }
+        if (x == 0)
+        {
+            printf("\nCestitke\n");
+        }else if (steviloPoskusov == 0) 
+        {
+            printf("\nZgubili ste, beseda je bila: %s\n", besede[index]);
+            x = 0;
         }
         
+        
+        
     }
-    if ((jeFloat >= 1) || (jeChar > 1) || (jeInt >1))
-        {
-            printf("Napacen vnos\n");
-            vnos(trenutnoNadstropje);
+    
 
-        }else if(jeChar == 1){
+}
 
-            if (jeInt == 1)
-            {
-                if((str[0] == 'b') || (str[0] == 'B') && (str[1] == '1')){
-                    if (str[1] == '1')
-                    {
-                        *trenutnoNadstropje = -3;
-                    }else if (str[1] == '2')
-                    {
-                        *trenutnoNadstropje = -2;
-                    }else if (str[1] == '3')
-                    {
-                        *trenutnoNadstropje = -1;
-                    }else{
-                    printf("Napacen vnos\n");
-                    vnos(trenutnoNadstropje);
-                }
-                }else{
-                    printf("Napacen vnos\n");
-                    vnos(trenutnoNadstropje);
-                }
-                
-            }else{
-                character = str[0];
+int str_dolzina(char str[]) {
+    int count; 
+    
+    for (count = 0; str[count] != '\0'; ++count);
+    
+    return count; 
+}
 
-                if((character == 'p') || (character == 'P')){
-                    *trenutnoNadstropje = 0;
-                }
-            
-                else{
-                    printf("Napacen vnos\n");
-                    vnos(trenutnoNadstropje);
-                }
-            }
-        }
-        else if (jeInt == 1){
+void izpis(char beseda[], int dolzina){
+    printf("\n");
 
-            intNum = atoi(str);
 
-            if ((intNum < 1) || (intNum > 5))
-            {
-                printf("Napacen vnos\n");
-                vnos(trenutnoNadstropje);
-            }else{
-                *trenutnoNadstropje = intNum;
-            }
-            
-        }
+    for (int i = 0; i < dolzina; i++)
+    {
+        
+        printf("%c ", beseda[i]);
+        
+    }
+    printf("\n");
+    printf("\n");
     
 }
 
-void konec(int *index){
+void update(int stPoskusov, char napacne[]){
+    printf("\n");
+    
+
+    printf("Stevilo poskusov: %i\n", stPoskusov);
+    printf("Napacne crke: ");
+
+    int velikostPolja = str_dolzina(napacne);
+
+    for (int i = 0; i < velikostPolja; i++)
+    {
+        printf("%c ", napacne[i]);
+    }
+    printf("\n");
+
+    printf("\n");
+}
+
+void vnos(int *stPoskusov, char napacne[], char beseda[], int dolzinaBesede, char *vnesenaCrka, int *napacenVnos){
+
     char input;
-    printf("\n");
-    printf("Ali bi zeleli izstopiti? --> (D/N) ");
-    scanf("%s", &input);
-    if ((input == 'D') || (input == 'd'))
+
+    printf("Vpisite crko: ");
+    scanf("%c", &input);
+
+    if (isalpha(input) == 0)
     {
-        printf("\n");
-        printf("Nasvidenje\n");
-        exit(0);
-    }else if ((input == 'N') || (input == 'n'))
-    {
-        *index = 0;
+        printf("\nNapacen vnos\n");
+        *napacenVnos = 1;
     }else{
-        printf("Napacen vnos\n");
-        konec(index);
-    }
-    printf("\n");
     
-}
+    
 
-void izpisPotnikov(int *stPotnikov){
-    printf("Trenutno stevilo potnikov: %i\n", *stPotnikov);
-}
+    int pravilno = 0;
 
-void izstopPotnikov(int *trenutnoNadstropje, int *stPotnikov, int index){
-    int st;
-    srand(time(0));
-    st = (rand() % 2) + 1;
-
-    if (*trenutnoNadstropje != 0)
+    for (int i = 0; i < dolzinaBesede; i++)
     {
-        if (st == 2)
+
+        if (beseda[i] == input)
         {
-            
-            //Nakljucno nadstropje na kateremu 1 ali vec potnikov izstopi
-            int naklNad;
-            srand(time(0));
-            if (*trenutnoNadstropje < 0)
-            {
-                naklNad = (rand() % (*trenutnoNadstropje + 1)) + *trenutnoNadstropje;
-            }else{
-                naklNad = (rand() % (*trenutnoNadstropje + 1));
-            }
-            printf("\n");
-
-            if (naklNad == -3)
-            {
-                printf("Ustavili smo se v kleti B1\n");
-            }else if (naklNad == -2)
-            {
-                printf("Ustavili smo se v kleti B2\n");
-            }else if (naklNad == -1)
-            {
-                printf("Ustavili smo se v kleti B3\n");
-            }else if (naklNad == 0)
-            {
-                printf("Ustavili smo se v pritlicju\n");
-            }else{
-                printf("Ustavili smo se na %i. nadstropju\n", naklNad);
-            }
-
-            //Nakljucno stevilo potnikov ki izstopi
-            int koliko;
-            srand(time(0));
-            koliko = (rand() % ((*stPotnikov - 1) - 1 + 1)) + 1;
-            *stPotnikov = *stPotnikov - koliko;
-            izpisPotnikov(stPotnikov);
-
-            konec(&index);
-
+            pravilno++;
+            *vnesenaCrka = input;
         }
+               
     }
 
+    if (pravilno == 0)
+    {
+        printf("Napacno\n");
+        strncat(napacne, &input, 1);
+        *stPoskusov = *stPoskusov - 1;
+    }else{
+        printf("Pravilno\n");
+    }
     
+    printf("\n");
+    }
 }
